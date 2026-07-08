52d5fd706effd51b74928425672e8b10bf24568f
athur15 <147349673+athur15@users.noreply.github.com>
2026-07-09 04:15:17 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,6 +6,9 @@ CHAT_ID = os.getenv("CHAT_ID")
 
 message = """🔔 Đến giờ kiểm tra trạng thái kênh.
 
+✔ Nếu không có thông báo biến động hoặc bảo trì
+✔ Sau thông báo khôi phục 2 tiếng
+
 ✔ Kiểm tra trạng thái kênh
 ✔ Nếu kênh ổn định thì phát thông báo
 """
